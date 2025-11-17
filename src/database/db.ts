de40d84485c0b5716c06c5b46dd240d07caec116
Sebastian Hernandez <sebastianabelhernandezarango@gmail.com>
2025-11-17 18:15:50 -0500

    V1.1

--- src/database/db.ts
+++ src/database/db.ts
@@ -9,8 +9,6 @@ export const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
   ssl: {
     rejectUnauthorized: false
   }
